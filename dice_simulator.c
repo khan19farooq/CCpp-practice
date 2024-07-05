4e3f134cea1d66300d55224814292e245a5bdcc6
khan19farooq <fahrukh.khan666@gmail.com>
2024-07-05 11:11:09 -0400

    String concantenation & flip letter

---
+++ dice_simulator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time (NULL)); // random number generator at current time
+
+    int dice = 5;
+    int roll = 0;
+
+    for (int i = 0; i <= 6; i++)
+    {
+        roll = rand() % 6 + 1;
+        printf("Dice %d: %d\n", i , roll);
+    }
+
+    return 0;
+}
--- stringConcatenation.c
+++ stringConcatenation.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h> 
+#include <ctype.h>
 
 char *string_append(char *s1, char *s2);
+void flip_letter(char *s);
 
 int main ()
 {
     char first[20] = "first ";
     char second[20] = "second";
 
-    strcat(first, second);
+    strcat(first, second);  // This is a function to concatenate 2 strings and store in first 
     printf("\nfirst: %s\n\n",first);
     
     char s1[] = "abc";
@@ -17,8 +19,16 @@ int main ()
 
     char *s = string_append(s1, s2);
     printf("\ns: %s\n\n", s);
-    free(s);
+    free(s); // Free memory after use 
+
+    char s3[] = "FahRukh";
+    char s4[] = "khan";
     
+    flip_letter(s3);
+    printf("s3 after: %s\n",s3);
+    flip_letter(s4);
+    printf("s4 after: %s\n",s4);
+
     return 0;
 }
 
@@ -40,7 +50,7 @@ char *string_append(char *s1, char *s2)
     int s2_length = strlen(s2);
     int size = s1_length + s2_length +1;
     
-    char *s = calloc (size, sizeof(char));
+    char *s = calloc (size, sizeof(char)); // Go get space on the heap -> calloc
 
     for (int i = 0; i < s1_length; i++)
         s[i] = s1[i];
@@ -51,4 +61,17 @@ char *string_append(char *s1, char *s2)
     s[size -1] = '\0';
 
     return s;
+}
+
+void flip_letter(char *s)
+{
+    int length  = strlen(s);
+
+    for (int i = 0; i < length; i++)
+    {
+        if (isupper (s[i]))
+            s[i] = tolower(s[i]);
+        else if (islower (s[i]))
+            s[i] = toupper(s[i]);
+    }
 }
